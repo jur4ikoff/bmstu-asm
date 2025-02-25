d184bfa9b05a84611218f2f8c046933a4d1012e7
1 <ypopov2005@gmail.com>
2025-02-25 14:28:38 +0300

    add code for 2 lab, update structs, update gitignore

---
+++ lab_02/main.asm
@@ -0,0 +1,27 @@
+stackSEG SEGMENT PARA STACK 'STACK'
+    DB 200h DUP (?)
+stackSEG ENDS
+
+dataSEG SEGMENT WORD 'DATA'
+HelloMessage DB 13
+    DB 10
+    DB 'Hello World!'
+    DB '$'
+dataSEG ENDS
+
+codeSEG SEGMENT WORD 'CODE'
+    ASSUME CS:codeSEG, DS:dataSEG
+dispMsg:
+    mov ax, dataSEG
+    mov ds, ax
+
+    mov dx, offset HelloMessage
+    mov ah, 9
+    int 21h
+    int 21h
+    int 21h
+
+    mov ah, 4ch
+    int 21h
+codeSEG ENDS
+END dispMsg
