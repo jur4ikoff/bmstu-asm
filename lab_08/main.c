c04185a8f65e342714f15d7a162b0ac14c173488
Linux <ypopov2005@gmail.com>
2025-04-20 23:33:06 +0300

    test

--- lab_08/main.c
+++ lab_08/main.c
@@ -1,19 +1,69 @@
 #include <gtk/gtk.h>
+#include <stdlib.h>
 
 #define WINDOW_TITLE "ЛР8 по МЗЯП. Попов Ю.А."
-#define LABEL_TEXT "Введите текст: "
+#define LABEL_TEXT "Введитеё текст: "
 #define BUTTON_TEXT "Найти ближайшую сверху степень двойки"
 #define RESULT_TEXT "Результат"
+#define ERR_NUMBER_TEXT "Ошибка, нужно ввести безнаковое число"
 #define WIDTH 300
 #define HEIGHT 150
+
+#define ERR_OK 0
+#define ERR_NUMBER 1
+
+/*
+int pow = find_nearest_power(num);
+        GtkWidget *msg_dialog = gtk_message_dialog_new(
+            NULL,
+            GTK_DIALOG_MODAL,
+            GTK_MESSAGE_INFO,
+            GTK_BUTTONS_OK,
+            "Ближайшая степень двойки ≥ %lld:\n2^%d = %lld", num, pow, (1LL << pow));
+        gtk_window_set_title(GTK_WINDOW(msg_dialog), "Результат");
+        g_signal_connect(msg_dialog, "response", G_CALLBACK(on_dialog_response), NULL);
+        gtk_widget_show_all(msg_dialog);
+*/
+
+static void on_dialog_response(GtkDialog *dialog, gint response_id, gpointer user_data)
+{
+    gtk_widget_destroy(GTK_WIDGET(dialog));
+}
+
+int find_upper_degree_of_two(long long num)
+{
+    int a = 1;
+    int count = 0;
+    while (a < num)
+    {
+        a <<= 1;
+        count++;
+    }
+
+    return count;
+}
 
 static void on_button_clicked(GtkWidget *widget, gpointer data)
 {
     GtkWidget *entry = (GtkWidget *)data;
     GtkWidget *label = g_object_get_data(G_OBJECT(entry), "label");
 
-    const gchar *text = gtk_entry_get_text(GTK_ENTRY(entry));
-    gtk_label_set_text(GTK_LABEL(label), text);
+    const char *text = gtk_entry_get_text(GTK_ENTRY(entry));
+
+    char *end = NULL;
+    long long number = strtoll(text, &end, 10);
+
+    if (*end != 0 || number < 0)
+    {
+        GtkWidget *msg_dialog = gtk_message_dialog_new(
+            NULL, GTK_DIALOG_MODAL, GTK_MESSAGE_INFO, GTK_BUTTONS_OK, ERR_NUMBER_TEXT);
+        gtk_window_set_title(GTK_WINDOW(msg_dialog), "Ошибка");
+        g_signal_connect(msg_dialog, "response", G_CALLBACK(on_dialog_response), NULL);
+        gtk_widget_show_all(msg_dialog);
+    }
+
+    int degree = find_upper_degree_of_two(number);
+    printf("%d\n", degree);
 }
 
 int main(int argc, char **argv)
@@ -23,7 +73,6 @@ int main(int argc, char **argv)
     GtkWidget *button;
     GtkWidget *label;
     GtkWidget *entry;
-    GtkWidget *res;
 
     gtk_init(&argc, &argv);
 
@@ -45,11 +94,6 @@ int main(int argc, char **argv)
     button = gtk_button_new_with_label(BUTTON_TEXT);
     gtk_grid_attach(GTK_GRID(grid), button, 0, 1, 2, 1);
 
-    res = gtk_label_new(RESULT_TEXT);
-    gtk_grid_attach(GTK_GRID(grid), res, 0, 2, 1, 1);
-
-    // Сохраняем указатель на label в entry (чтобы получить его в callback)
-    g_object_set_data(G_OBJECT(entry), "label", res);
     g_signal_connect(button, "clicked", G_CALLBACK(on_button_clicked), entry);
 
     gtk_widget_show_all(window);
