12b5a65b3d16269f41ab0b8a8b5bda6ed14667f8
Linux <ypopov2005@gmail.com>
2025-04-29 19:40:07 +0300

    update

--- lab_10/task_1/build_no_coprocessor.sh
+++ lab_10/task_1/build_no_coprocessor.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-gcc -mno-80387 -O0 -o app_no_coprocessor_test.exe main.c
+# gcc -mno-80387 -O0 -o app_no_coprocessor_test.exe main.c
+gcc -O0 -o app_no_coprocessor_test.exe main.c
 # -mno-80387	Запрещает использование x87 FPUx
