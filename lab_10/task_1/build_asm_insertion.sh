7683a57b6af706f2d6935d7fcdc046426e1ae6c4
1 <ypopov2005@gmail.com>
2025-04-29 20:57:32 +0300

    finish first task

--- lab_10/task_1/build_asm_insertion.sh
+++ lab_10/task_1/build_asm_insertion.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-gcc -O0 -masm=intel -o app_asm_insertion.exe insert_asm.c
+gcc -mfpmath=387 -O0 -masm=intel -o app_asm_insertion.exe insert_asm.c
 # -mfpmath=387 — заставляет GCC использовать x87 для float/double. 
--- lab_10/task_1/insert_asm.c
+++ lab_10/task_1/insert_asm.c
@@ -1,16 +1,75 @@
 #include <stdio.h>
+#include <time.h>
+
+#define MAX_ITTERATIONS 10000000
+
+void test_float_operations(void)
+{
+    float a = 1.234567f, b = 7.890123f, res;
+
+    clock_t start = clock();
+
+    for (int i = 0; i < MAX_ITTERATIONS; i++)
+    {
+        // Сложение
+        asm volatile(
+            "fld %1\n\t"     // Загружаем первое число в стек FPU (ST(0))
+            "fadd %2\n\t"    // Прибавляем второе число к ST(0)
+            "fstp %0\n\t"    // Сохраняем результат из ST(0) в res и выталкиваем из стека
+            : "=m"(res)      // Выходной операнд - в память
+            : "m"(a), "m"(b) // Входные операнды - из памяти
+            : "st"           // Указываем, что используем регистр ST(0)
+        );
+
+        // Умножение
+        asm volatile(
+            "fld %1\n"
+            "fmul %2\n"
+            "fstp %0\n"
+            : "=m"(res)
+            : "m"(a), "m"(b)
+            : "st");
+    }
+    clock_t end = clock();
+    double time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+    printf("Время float операций: %.6f секунд, Количество иттераций %d\n", time_used, MAX_ITTERATIONS);
+}
+
+void test_double_operations(void)
+{
+    double a = 1.23456342423432447, b = 7.8901234314315234, res;
+
+    clock_t start = clock();
+
+    for (int i = 0; i < MAX_ITTERATIONS; i++)
+    {
+        // Сложение
+        asm volatile(
+            "fld %1\n\t"     // Загружаем первое число в стек FPU (ST(0))
+            "fadd %2\n\t"    // Прибавляем второе число к ST(0)
+            "fstp %0\n\t"    // Сохраняем результат из ST(0) в res и выталкиваем из стека
+            : "=m"(res)      // Выходной операнд - в память
+            : "m"(a), "m"(b) // Входные операнды - из памяти
+            : "st"           // Указываем, что используем регистр ST(0)
+        );
+
+        // Умножение
+        asm volatile(
+            "fld %1\n"
+            "fmul %2\n"
+            "fstp %0\n"
+            : "=m"(res)
+            : "m"(a), "m"(b)
+            : "st");
+    }
+    clock_t end = clock();
+    double time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+    printf("Время double операций: %.6f секунд, Количество иттераций %d\n", time_used, MAX_ITTERATIONS);
+}
 
 int main(void)
 {
-    int a = 5, b = 10, result;
-
-    __asm volatile(
-        "add %1, %2\n"   // %1 + %2 → %2
-        "mov %2, %0"     // %2 → %0
-        : "=r"(result)   // %0
-        : "r"(a), "r"(b) // %1 и %2
-        : "cc");
-
-    printf("%d\n", result);
+    test_float_operations();
+    test_double_operations();
     return 0;
 }
